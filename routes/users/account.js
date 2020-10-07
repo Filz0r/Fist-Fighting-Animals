5e7087b648ffc623e6b8c83323468a1d74953a74
Filipe Figueiredo <filipefig26@gmail.com>
2020-10-07 05:12:26 +0100

    created an post route
    this route fixes the issue found on users/account.ejs

--- routes/users/account.js
+++ routes/users/account.js
@@ -1,11 +1,32 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcrypt')
 const { checkAuthenticated } = require('../../controllers/AuthController')
 const User = require('../../schemas/userSchema')
 
 router.get('/', checkAuthenticated, async (req, res) => {
     const path = req.originalUrl
     res.render('users/account', { user: req.user, path: path })
+})
+router.post('/:id', checkAuthenticated, async (req, res) => {
+    const { newPw, checkNewPw } = req.body
+        if (await bcrypt.compare(newPw, req.user.password)) {
+            req.flash('changes', 'password is the same as old one')
+            res.redirect(`/admin/edit/${req.params.id}`)
+        } else if (newPw !== checkNewPw) {
+            req.flash('changes', 'new passwords do not match!')
+            res.redirect(`/admin/edit/${req.params.id}`)
+        } else {
+            const hashedPW = await bcrypt.hash(newPw, 10)
+            const pass = newPw.length > 0 ? hashedPW : req.user.password
+            await User.findByIdAndUpdate({ _id: req.params.id }, {
+                username: req.body.username,
+                email: req.body.email,
+                password: pass
+            })
+            req.flash('changes', 'User edited!')
+            res.redirect('/account')
+        }
 })
 
 module.exports = router
