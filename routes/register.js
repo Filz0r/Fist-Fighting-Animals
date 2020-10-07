52f9eeebf73409fabee0fb45751cab29959dbb14
Filipe Figueiredo <filipefig26@gmail.com>
2020-10-07 17:43:30 +0100

    made sure that the email that is registered is always in lowercase before being saved to the db, and that the try catch that saves the user only runs if the password passes a couple of requirements

--- routes/register.js
+++ routes/register.js
@@ -13,36 +13,42 @@ router.post('/', checkNotAuthenticated, async (req, res) => {
     if(password !== password2) {
         req.flash('message', 'Passwords do not match!')
         return res.redirect('/register')
-    }
-    try {
-        const hashedPw = await bcrypt.hash(password, 10)
-        const id = await User.countDocuments()
-        User.findOne({ email: email }).then((user) => {
-            if (user) {
-                req.flash('message', 'That email already exists!')
-                res.redirect('/register')
-            } else {
-                new User({
-                    _id: id,
-                    username,
-                    email,
-                    password: hashedPw,
-                    admin: false,
-                    level: 1,
-                    attack: 1,
-                    defense: 1,
-                    HP: 10,
-                    storyLvl: 1,
-                    pointsToAdd: 0,
-                    coins: 0
-                }).save().then( () => {
-                    req.flash('message', 'You are now registered!')
-                    res.redirect('/login')
-                })
-            }
-        })
-    } catch (e) {
-        console.log(e)
+    } else if (password.length < 6) {
+        req.flash('message', 'Password must have more than 6 charecters!')
+        return res.redirect('/register')
+    } else {
+        try {
+            const hashedPw = await bcrypt.hash(password, 10)
+            const id = await User.countDocuments()
+            User.findOne({ email: email }).then((user) => {
+                if (user) {
+                    req.flash('message', 'That email already exists!')
+                    res.redirect('/register')
+                } else {
+                    new User({
+                        _id: id,
+                        username,
+                        email: email.toLowerCase(),
+                        password: hashedPw,
+                        admin: false,
+                        level: 1,
+                        attack: 5,
+                        defense: 5,
+                        HP: 10,
+                        storyLvl: 1,
+                        pointsToAdd: 0,
+                        coins: 0,
+                        storyCounter: 1,
+                        new: true,
+                    }).save().then( () => {
+                        req.flash('message', 'You are now registered!')
+                        res.redirect('/login')
+                    })
+                }
+            })
+        } catch (e) {
+            console.log(e)
+        }
     }
 })
 
