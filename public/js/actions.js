a6139df6cee40c81d0764128cef9d5bbdd12d562
Filipe Figueiredo <filipefig26@gmail.com>
2020-10-07 17:42:10 +0100

    finished the fight actions script

--- public/js/actions.js
+++ public/js/actions.js
@@ -1,17 +1,26 @@
+
+$('#animalBar').width('100%')
+$('#userBar').width('100%')
 function fight(action) {
     const animal = {
         name: $('#animalName').html(),
-        level: + $('#animalLevel').html().replace(/,/g, ''),
+        level: + $('#animalLevel').html().substring(4).replace(/,/g, ''),
         attack: + $('#animalAttack').html().replace(/,/g, ''),
         defense: + $('#animalDefense').html().replace(/,/g, ''),
-        hp: + $('#animalHP').html().replace(/,/g, '')
+        hp: + $('#animalHP').html().replace(/,/g, ''),
+        fixHP: + $('#animalHpFixed').html().substring(1).replace(/,/g, '')
     }
     const user = {
         name: $('#userName').html(),
-        level: + $('#userLevel').html().replace(/,/g, ''),
+        level: + $('#userLevel').html().substring(4).replace(/,/g, ''),
         attack: + $('#userAttack').html().replace(/,/g, ''),
         defense: + $('#userDefense').html().replace(/,/g, ''),
-        hp: + $('#userHP').html().replace(/,/g, '')
+        hp: + $('#userHP').html().replace(/,/g, ''),
+        fixHP: + $('#userHpFixed').html().substring(1).replace(/,/g, '')
+    }
+    function hpPercent(val, user){
+        const result = (val * 100) / user.fixHP
+        return `${result}%`
     }
     if (action == 'attack') {
         animal.hp = Math.round(animal.hp - (user.attack * 0.65 + animal.defense * 0.35))
@@ -19,31 +28,53 @@ function fight(action) {
         if(animal.hp <= 0 && user.hp > 0) {
             $('#animalHP').html('0')
             $('#userHP').html(user.hp)
+            $('#animalBar').width('0%')
+            $('#userBar').width(hpPercent(user.hp, user))
             $('#winModal').show()
-            document.body.className = 'modal-open'
         } else if (animal.hp > 0 && user.hp > 0) {
             $('#animalHP').html(animal.hp)
+            $('#animalBar').width(hpPercent(animal.hp, animal))
+            $('#userBar').width(hpPercent(user.hp, user))
             $('#userHP').html(user.hp)
         } else if (animal.hp > 0 && user.hp <= 0) {
             $('#animalHP').html(animal.hp)
             $('#userHP').html('0')
+            $('#animalBar').width('0%')
+            $('#userBar').width(hpPercent(user.hp, user))
             $('#lossModal').show()
-        }        
+        } else if ( animal.hp<= 0 && user.hp <= 0) {
+            $('#animalHP').html('0')
+            $('#userHP').html('0')
+            $('#animalBar').width('0%')
+            $('#userBar').width('0%')
+            $('#lossModal').show()
+        }    
     } else if (action == 'defend') {
         animal.hp = Math.round(animal.hp - (user.defense * 0.45 + animal.defense * 0.55))
         user.hp = Math.round(user.hp - (user.defense * 0.05 + animal.attack * 0.95))
         if(animal.hp <= 0 && user.hp > 0) {
             $('#animalHP').html('0')
             $('#userHP').html(user.hp)
+            $('#animalBar').width('0%')
+            $('#userBar').width(hpPercent(user.hp, user))
             $('#winModal').show()
-            document.body.className = 'modal-open'
         } else if (animal.hp > 0 && user.hp > 0) {
             $('#animalHP').html(animal.hp)
+            $('#animalBar').width(hpPercent(animal.hp, animal))
+            $('#userBar').width(hpPercent(user.hp, user))
             $('#userHP').html(user.hp)
         } else if (animal.hp > 0 && user.hp <= 0) {
             $('#animalHP').html(animal.hp)
             $('#userHP').html('0')
+            $('#animalBar').width('0%')
+            $('#userBar').width(hpPercent(user.hp, user))
             $('#lossModal').show()
-        }
+        } else if ( animal.hp<= 0 && user.hp <= 0) {
+            $('#animalHP').html('0')
+            $('#userHP').html('0')
+            $('#animalBar').width('0%')
+            $('#userBar').width('0%')
+            $('#lossModal').show()
+        } 
     }
 }
