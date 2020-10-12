b735819182480fa1e3b66aaaf6eb85c31b43d0b3
Filipe Figueiredo <filipefig26@gmail.com>
2020-10-12 03:20:48 +0100

    Update fight.js
    still in development

--- routes/users/fight.js
+++ routes/users/fight.js
@@ -1,25 +1,32 @@
 const express = require('express')
 const router = express.Router()
 const { checkAuthenticated } = require('../../controllers/AuthController')
+const { itemGiver } = require('../../controllers/itemHandler')
 const Animal = require('../../schemas/animalSchema')
 const User = require('../../schemas/userSchema')
+//const Items = require('../../schemas/itemSchema')
+
 
 router.get('/', checkAuthenticated, async (req, res) => {
     const animals = await Animal.find({ storyLvl: req.user.storyLvl })
-    const currentAnimal = animals[req.user.storyCounter - 1]
+    const currentAnimal = animals[req.user.storyCounter]
+    //console.log(`CurrentAnimal: ${currentAnimal}`)
     const path = req.originalUrl
     res.render('users/fight', { user: req.user, path: path, animal: currentAnimal })
-
+    itemGiver(req.user.id)
 })
 router.post('/', checkAuthenticated, async (req, res) => {
     const animals = await Animal.find({ storyLvl: req.user.storyLvl })
     const { pointsToAdd, coinsDrop } = animals[req.user.storyCounter - 1]
     let { _id, pointsToAdd: points, coins, storyCounter, storyLvl } = await User.findById({ _id: req.user.id })
-    if (storyCounter >= 5) {
+    const storyLvlChanger = storyCounter % 5
+    console.log(`LVL CHANGER:${storyLvlChanger}`)
+    if (storyLvlChanger == 0) {
         points += pointsToAdd
         coins += coinsDrop
-        storyCounter = 1
+        storyCounter += 1
         storyLvl += 1
+        itemGiver(req.user.id)
         await User.findByIdAndUpdate({ _id }, {
             pointsToAdd: points,
             coins,
@@ -30,6 +37,7 @@ router.post('/', checkAuthenticated, async (req, res) => {
         points += pointsToAdd
         coins += coinsDrop
         storyCounter += 1
+        itemGiver(req.user.id)
         await User.findByIdAndUpdate({ _id }, {
             pointsToAdd: points,
             coins,
