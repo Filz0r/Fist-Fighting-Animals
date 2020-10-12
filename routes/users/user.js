164fb8dc87630be7ea24c73a02f0c892f704736c
Filipe Figueiredo <filipefig26@gmail.com>
2020-10-12 03:20:55 +0100

    Update user.js

--- routes/users/user.js
+++ routes/users/user.js
@@ -1,21 +1,23 @@
 const express = require('express')
 const router = express.Router()
 const { checkAuthenticated } = require('../../controllers/AuthController')
+const { bagSorter, itemChecker } = require('../../controllers/itemHandler')
 const User = require('../../schemas/userSchema')
 
 router.get('/', checkAuthenticated, async (req, res) => {
     const path = req.originalUrl
-    res.render('users/user', { user: req.user, path: path })
+    const bag = await bagSorter(req.user.id)
+    //const items = await itemChecker(req.user.id)
+    //console.log(items)
+    res.render('users/user', { user: req.user, bag: bag, path: path })
 })
 router.post('/:id', checkAuthenticated, async (req, res) => {
     const id = req.params.id
-    const { userLevel: level, userStory: storyLvl, userPoints: pointsToAdd, userAttack: attack, userDefense: defense, userHP: HP } = req.body
+    const { userPoints: pointsToAdd, userAttack: attack, userDefense: defense, userHP: HP } = req.body
     await User.findByIdAndUpdate({ _id: id }, {
-        level,
         attack,
         defense,
         HP,
-        storyLvl,
         pointsToAdd
     })
     res.redirect('/user')
