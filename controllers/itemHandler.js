7fd34090608e3126178c1b8618b02b0f9657a197
Filipe Figueiredo <filipefig26@gmail.com>
2020-10-13 01:06:50 +0100

    Update itemHandler.js
    this should handle all the item related functions for the app, it might still have a few bugs, to be tested as the app grows

--- controllers/itemHandler.js
+++ controllers/itemHandler.js
@@ -4,22 +4,30 @@ const Animals = require('../schemas/animalSchema')
 
 async function itemGiver(userId) {
     const itemIdCheck = await itemIdChecker(userId)
-    const equipArr = await itemsInBagEquipChecker(itemIdCheck)
-    
-    
-    let { storyLvl, storyCounter } = await User.findById({ _id: userId })
+    let { storyCounter } = await User.findById({ _id: userId })
     const { itemsToDrop } = await Animals.findById({ _id: storyCounter })
-    
+
     //creates the objects that are to be inserted in the database
-    const itemsToUpload = async(itemsToDrop)  => {
+    const itemsToUpload = async (itemsToDrop) => {
         let i = 0
-        const itemDropData = []
-        //let dropCheck = true
+        const toUpload = {
+            itemUpdateData: [],
+            itemDropData: []
+        }
         do {
             const idOfItemsToDrop = itemsToDrop[i]
             const { name, category, effects, equipable, _id: id } = await Items.findById({ _id: idOfItemsToDrop })
-            //this will add items to the users bag if they dont have it
-            if (typeof itemIdCheck[i] == 'undefined') {
+            // this will check if the items exist in the users bag if they do it will create a second object that will increase the ammount of consumable items
+            if (itemIdCheck.length > 0 || itemIdCheck.includes(id) ) {
+                const item = {
+                    id,
+                    name,
+                    category,
+                    equipable
+                }
+                toUpload.itemUpdateData.push(item)
+                i++
+            } else {
                 const item = {
                     id,
                     name,
@@ -27,43 +35,101 @@ async function itemGiver(userId) {
                     effects,
                     equipable
                 }
-                itemDropData.push(item) 
-            i++
-            } else {
+                toUpload.itemDropData.push(item)
                 i++
-                //dropCheck = false
             }
         } while (i < itemsToDrop.length)
-        return itemDropData
+        return toUpload
     }
-    const test = await itemsToUpload(itemsToDrop)
-    console.log(test)
+    //uploads the objects to the database
+    const itemUploader = async (toUpload) => {
+        if (toUpload.itemDropData.length > 0 && toUpload.itemUpdateData.length <= 4) {
+            let n = 0
+            do {
+                if (toUpload.itemDropData[n].category == 'armor') {
+                    const itemToGive = {
+                        id: toUpload.itemDropData[n].id,
+                        name: toUpload.itemDropData[n].name,
+                        category: toUpload.itemDropData[n].category,
+                        effects: toUpload.itemDropData[n].effects,
+                        equipable: toUpload.itemDropData[n].equipable,
+                        equiped: false
+                    }
+                    await User.findByIdAndUpdate({ _id: userId }, {
+                        $push: {
+                            bag: itemToGive
+                        }
+                    })
+                } else if (toUpload.itemDropData[n].category == 'consumable') {
+                    const itemToGive = {
+                        id: toUpload.itemDropData[n].id,
+                        name: toUpload.itemDropData[n].name,
+                        category: toUpload.itemDropData[n].category,
+                        effects: toUpload.itemDropData[n].effects,
+                        equipable: toUpload.itemDropData[n].equipable,
+                        quantity: 1
+                    }
+                    await User.findByIdAndUpdate({ _id: userId }, {
+                        $push: {
+                            bag: itemToGive
+                        }
+                    })
+                } else if (toUpload.itemDropData[n].category == 'keyItems') {
+                    console.log(toUpload.itemDropData[n].name)
+                }
+                n++
+            } while (n < toUpload.itemDropData.length)
+            if(toUpload.itemUpdateData.length < 1) return
+            i = 0
+            do {
+                if (!toUpload.itemUpdateData[i].equipable) {
+                    let { bag } = await User.findById({
+                        _id: userId
+                    })
+                    const bagObj = Object.values(bag)
+                    const indexOfItem = bagObj.findIndex(n => n.id == toUpload.itemUpdateData[i].id)
+                    bag[indexOfItem].quantity += 1
+                    await User.findByIdAndUpdate({ _id: userId }, {
+                        bag
+                    })
+                }
+                i++
+            } while (i < toUpload.itemUpdateData.length)
+            return true
+        } else if (toUpload.itemDropData.length < 1 && toUpload.itemUpdateData.length <= 4) {
+            i = 0
+            do {
+                if (!toUpload.itemUpdateData[i].equipable) {
+                    let { bag } = await User.findById({
+                        _id: userId
+                    })
+                    const bagObj = Object.values(bag)
+                    const indexOfItem = bagObj.findIndex(n => n.id == toUpload.itemUpdateData[i].id)
+                    bag[indexOfItem].quantity += 1
+                    await User.findByIdAndUpdate({ _id: userId }, {
+                        bag
+                    })
+                }
+                i++
+            } while (i < toUpload.itemUpdateData.length)
+            return true
+        }
+    }
+
+    const toUpload = await itemsToUpload(itemsToDrop)
     
-    
-    
+    return await itemUploader(toUpload)
 }
 
 //grabs the ID's of all the items the user has in his bag
 async function itemIdChecker(userId) {
     const { bag } = await User.findById({ _id: userId })
-    if (bag.length < 1) return false
+    if (bag.length < 1) return bag
     const bagCheck = Object.values(bag).map((item) => {
         const id = item.id
         return id
     })
     return bagCheck
-}
-//Checks if the items the user has in his bag can be equiped or not, returns an array of true or false
-async function itemsInBagEquipChecker(ids) {
-    if (ids === false) return
-    const result = []
-    let i = 0
-    do {
-        const { equipable } = await Items.findById({ _id: ids[i] })
-        result.push(equipable)
-        i++
-    } while (i < ids.length)
-    return result
 }
 
 async function bagSorter(userId) {
@@ -73,77 +139,19 @@ async function bagSorter(userId) {
     const sortedBag = []
     let i = 0
     do {
-        const itemsInBag = {
+       const itemsInBag = {
             name: bagToSort[i].name,
             category: bagToSort[i].category,
             equipable: bagToSort[i].equipable,
             effects: bagToSort[i].effects,
-            equiped: bagToSort[i].equiped
+            equiped: typeof bagToSort[i].equiped == 'undefined' ? null : bagToSort[i].equiped
         }
-        i++
         sortedBag.push(itemsInBag)
-    } while (bagToSort.length < i)
+        i++
+    } while (i < bagToSort.length)
+    console.log(sortedBag)
     return sortedBag
 }
 
-module.exports = { itemGiver, itemIdChecker, bagSorter, itemsInBagEquipChecker }
+module.exports = { itemGiver, itemIdChecker, bagSorter }
 
-//if the user has no items in the bag
-    /*if (typeof equipArr == 'undefined') {
-        i = 0
-        do {
-            if (items[i].category == 'armor') {
-                const itemToGive = {
-                    id: items[i].id,
-                    name: items[i].name,
-                    category: items[i].category,
-                    effects: items[i].effects,
-                    equipable: items[i].equipable,
-                    equiped: false
-                }
-                await User.findByIdAndUpdate({ _id: userId }, {
-                    $push: {
-                        bag: itemToGive
-                    }
-                })
-            }
-            if (items[i].category == 'consumable') {
-                const itemToGive = {
-                    id: items[i].id,
-                    name: items[i].name,
-                    category: items[i].category,
-                    effects: items[i].effects,
-                    equipable: items[i].equipable,
-                    quantity: 1
-                }
-                await User.findByIdAndUpdate({ _id: userId }, {
-                    $push: {
-                        bag: itemToGive
-                    }
-                })
-            }
-            i++
-        } while (i < items.length)
-
-    } else {
-        i = 0
-        do {
-            if (typeof items[i] == 'undefined') {
-                i++
-            } else {
-                //if the item is not equipable increase its quantity
-                if (!equipArr[i]) {
-                    let { bag } = await User.findById({
-                        _id: userId
-                    })
-                    const bagObj = Object.values(bag)
-                    const indexOfItem = bagObj.findIndex(n => n.name === items[i].name)
-                    bag[indexOfItem].quantity += 1
-                    await User.findByIdAndUpdate({ _id: userId }, {
-                        bag
-                    })
-                }
-                i++
-            }
-        } while (i < items.length)
-    }*/
